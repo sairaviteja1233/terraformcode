b84c02cb0335dde3131e0595bdee3ef220250f3a
sairaviteja1233 <149599147+sairaviteja1233@users.noreply.github.com>
2024-12-12 12:44:07 +0530

    Update rg.tf

--- Desktop/Git code/batch15terra/rg.tf
+++ Desktop/Git code/batch15terra/rg.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "testResourceGroup1"
+  location = "West US"
+
+  tags = {
+    environment = "Production"
+  }
+}
